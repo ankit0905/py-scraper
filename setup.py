4668cb4357919a07b7a4ac7e195e5c8573f352c1
ankit <ankit0905anand@gmail.com>
2016-12-21 17:01:16 +0530

    basic structure added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='py-scraper'
+    version='1.0.0'
+    description='GUI tool for Web-Scraping'
+    long_description=readme
+    author='Ankit Anand'
+    author_email=''
+    url='https://github.com/ankit0905/py-scraper'
+)
